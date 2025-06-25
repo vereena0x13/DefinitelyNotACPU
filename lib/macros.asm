c8a08ce68ca2ca55ffb7b1a16a85490add5a39d8
Vereena Inara <vereena.inara@gmail.com>
2025-06-25 08:37:28 -0400

    ope

--- lib/macros.asm
+++ lib/macros.asm
@@ -3,18 +3,18 @@
 
 #ruledef {
     lda [{addr: u16}] => asm {
-        lda {addr}
-        sta a+2
         lda {addr}+1
+        sta a+2
+        lda {addr}
         sta a+1
 a:      lda 0x0000
     }
 
     sta [{addr: u16}] => asm {
         sta a-1
-        lda {addr}
-        sta a+2
         lda {addr}+1
+        sta a+2
+        lda {addr}
         sta a+1
         lda #0x00
 a:      sta 0x0000
