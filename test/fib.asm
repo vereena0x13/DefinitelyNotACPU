ec3b827083cb502dd318f6af8892a3c190b06fdc
Vereena Inara <vereena.inara@gmail.com>
2025-06-25 04:42:13 -0400

    yay!

--- test/fib.asm
+++ test/fib.asm
@@ -32,51 +32,65 @@
 }
 
 
-                    ;lcd_init
-                    ;lcd_clear
-                    ;lcd_goto 0, 0
 
 
-                    ;lda #0xFF
-                    ;and #0b11010110
-                    ;lda #0b10000010
-                    ;or #0b00101100
-                    ;lda #0xFF
-                    ;xor #0b01101110
+                    jmp start
 
 
-                    lda #0x01
 
-a:                  #res 256
-                    lsh
-                    #res 256
-                    lsh
-                    #res 256
-                    lsh
-                    #res 256
-                    lsh
-                    #res 256
-                    lsh
-                    #res 256
-                    lsh
-                    #res 256
-                    lsh
-                    #res 256
-                    rsh
-                    #res 256
-                    rsh
-                    #res 256
-                    rsh
-                    #res 256
-                    rsh
-                    #res 256
-                    rsh
-                    #res 256
-                    rsh
-                    #res 256
-                    rsh
-                    jmp a
+t0:                 #res 1
+tptr0:              #d16 HEX_DIGITS
+
+#align 16*32
+#assert (HEX_DIGITS & 0x000F) == 0
+HEX_DIGITS:         #d "0123456789ABCDEF"
+
+
+#ruledef {
+    lcd_write_hex_digit => asm {
+        add #(HEX_DIGITS & 0x00FF)
+        sta tptr0 + 1
+        lda [tptr0]
+        sta LCDDAT
+    }
+
+    lcd_write_hex_byte => asm {
+        sta t0
+        and #0xF0
+        rsh
+        rsh
+        rsh
+        rsh
+        lcd_write_hex_digit
+        lda t0
+        and #0x0F
+        lcd_write_hex_digit
+        lda t0
+    }
+}
+
+
+
+start:              lcd_init
+                    lcd_clear
+                    lcd_goto 0, 0
+
+
+                    ldz
+
+l00p:               #res 64
+                    
+                    sta t0
+                    lcd_goto 0, 0
+                    lda t0
+                    lcd_write_hex_byte
+                    
+                    add #1
+                    jmp l00p
 
 
 loop:               #res 64
                     jmp loop
+
+
+
