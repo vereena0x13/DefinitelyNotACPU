7e3e6a3b224f001bb31e0801e17a205fa9d2e4aa
Vereena Inara <vereena.inara@gmail.com>
2025-07-14 15:23:09 -0400

    Surely this will work right? lol

--- fpga/spinal/gay/vereena/cpu/CPU.scala
+++ fpga/spinal/gay/vereena/cpu/CPU.scala
@@ -8,44 +8,20 @@ import gay.vereena.cpu.Util._
 
 
 
-case class BusCmd() extends Bundle {
-    val addr    = UInt(16 bits)
-    val data    = UInt(8 bits)
-    val write   = Bool()
-}
-
-case class BusRsp() extends Bundle with IMasterSlave {
-    val data    = UInt(8 bits)
-
-    def asMaster(): Unit = {
-        out(data)
-    }
-}
-
-case class Bus() extends Bundle with IMasterSlave {
-    val cmd     = Stream(BusCmd())
-    val rsp     = BusRsp()
-
-    def asMaster(): Unit = {
-        master(cmd)
-        slave(rsp)
-    }
-}
-
-
 case class CPU() extends Component {
     val io = new Bundle {
-        val bus             = master(Bus())
+        val addr            = out UInt(16 bits)
+        val write_data      = out UInt(8 bits)
+        val read_data       = in UInt(8 bits)
+        val write           = out Bool()
     }
-    import io._
-
-
-    bus.cmd.write           := False
-    bus.cmd.valid.setAsReg() init(False)
 
 
     val addr                = UInt(16 bits)
+    addr                    := 0
     val data                = UInt(8 bits)
+    data                    := 0
+
 
     val upc                 = Reg(UInt(4 bits)) init(0)
     val pc                  = Reg(UInt(16 bits)) init(0)
@@ -58,6 +34,16 @@ case class CPU() extends Component {
         val c               = Reg(Bool()) init(False)
     }
 
+    val mar_lo              = UInt(8 bits)
+    mar_lo                  := 0
+    val mar_hi              = UInt(8 bits)
+    mar_hi                  := 0
+
+    val zero                = Bool()
+    zero                    := False
+    val carry               = Bool()
+    carry                   := False
+
 
     val ctrl    = new Bundle {
         // TODO: really we should just like, parse the microcode out of the ucode_xx.h files in cpu_ctrl.
@@ -66,7 +52,6 @@ case class CPU() extends Component {
         val uaddr           = flags.c ## flags.z ## ir(5 downto 0) ## upc
         val uinsn           = ucode.readAsync(uaddr.asUInt)
         
-        /*
         val d               = uinsn(7 downto 0)
         val d_rd            = uinsn(8)
         val alu_xorb        = uinsn(9)
@@ -92,14 +77,68 @@ case class CPU() extends Component {
         val d_to_mar_hi     = uinsn(28)
         val upc_inc         = uinsn(30)
         val alu_rd          = uinsn(31)
-        */
     }
 
 
+    when(ctrl.pc_rd)        { addr := pc }
+    when(ctrl.mar_rd)       { addr := mar }
+    
+    when(ctrl.a_rd)         { data := a }           
+    when(ctrl.addr_lo_to_d) { data := addr(7 downto 0) }   
+    when(ctrl.addr_hi_to_d) { data := addr(15 downto 8) }   
+    when(ctrl.ram_rd)       { data := io.read_data }         
 
+    when(ctrl.alu_rd) {
+        val sum             = UInt(9 bits)
+        val x               = (U(0, 1 bit) ## a).asUInt
+        val y               = (U(0, 1 bit) ## b).asUInt
+        val c               = ctrl.alu_ci.asUInt
+        when(ctrl.alu_xorb) {
+            sum             := x - y + c - 1
+            carry           := (sum & 0x100) === 0
+        } otherwise {
+            sum             := x + y + c
+            carry           := sum > 0xFF
+        }     
+        data                := sum(7 downto 0)
+        zero                := data === 0
+    }
 
+    when(ctrl.alu_bit_rd) {
+        switch(ir & 0b00000011) {
+            is(0) { data := (b << 1)(7 downto 0) }
+            is(1) { data := (b >> 1).expand }
+            default {
+                switch(ctrl.uinsn & 0x600) {
+                    is(0) { data := a & b }
+                    is(0x200) { data := a | b }
+                    is(0x400) { data := a ^ b }
+                    default {  }
+                }
+            }
+        }
+    }
 
-    bus.cmd.addr            := addr
-    bus.cmd.data            := data
-    // bus.cmd.write           := ctrl.ram_wr
+    when(ctrl.d_to_mar_lo)  { mar_lo := data }
+    when(ctrl.d_to_mar_hi)  { mar_hi := data }
+
+    when(ctrl.pc_ld)        { pc := addr }
+    when(ctrl.mar_lo_ld)    { mar(7 downto 0) := data }
+    when(ctrl.mar_hi_ld)    { mar(15 downto 8) := data }
+
+    when(ctrl.a_ld)         { a := data }
+    when(ctrl.b_ld)         { b := data }
+
+    when(ctrl.flags_ld)     {
+        flags.z             := zero
+        flags.c             := carry
+    }
+
+    when(ctrl.ir_ld)        { ir := data }
+    when(ctrl.upc_inc)      { upc := upc + 1 }
+    when(ctrl.upc_rst)      { upc := 0 }
+
+    io.addr                 := addr
+    io.write_data           := data
+    io.write                := ctrl.ram_wr
 }
