d81f4b12b688c000ad571e73eb25fa9cf97811c4
Vereena Inara <vereena.inara@gmail.com>
2025-07-23 13:36:21 -0400

    eas;dh

---
+++ compiler/src/main.cpp
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <float.h>
+#include <time.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#ifdef _MSC_VER
+#include <intrin.h>
+#else
+#include <x86intrin.h>
+#endif
+
+
+#define VSTD_IMPL
+#include "vstd.hpp"
+
+
+#include "util.cpp"
+#include "ctrl.h"
+#include "isa.h"
+#include "ucode.cpp"
+#include "dasm.cpp"
+#include "emu.cpp"
+
+
+
+void usage() {
+    printf("usage: cputool <file>\n");
+}
+
+s32 main(s32 argc, cstr *argv) {
+    if(argc < 2) {
+        usage();
+        return 1;
+    }
+
+    return emulate(argv[1]);
+}
